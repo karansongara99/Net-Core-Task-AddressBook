1b96df6388b2e03708a02d46d1675bb6232ab70c
Karan Songara <karansongara290@gmail.com>
2025-04-03 13:33:43 +0530

    Services folder Upload

---
+++ Adddress Book/Services/SessionService.cs
@@ -0,0 +1,22 @@
+﻿namespace CrudOperationEntityFrameWork.Services
+{
+    public class SessionService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public SessionService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetUserId()
+        {
+            return httpContextAccessor.HttpContext.Session.GetInt32(Constants.Constants.USERID_SESSION_KEY) ?? -1;
+        }
+
+        public string GetUserName()
+        {
+            return httpContextAccessor.HttpContext.Session.GetString(Constants.Constants.USERNAME_SESSION_KEY);
+        }
+    }
+}
